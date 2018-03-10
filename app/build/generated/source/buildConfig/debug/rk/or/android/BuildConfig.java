89916d652dcb689db8025565b397d34b7eac27de
yangxiaojian <yangxiaojian@didichuxing.com>
2018-03-10 20:44:54 +0800

    initial

---
+++ app/build/generated/source/buildConfig/debug/rk/or/android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package rk.or.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "rk.or.android";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "OriSim3D-1.5";
+}
